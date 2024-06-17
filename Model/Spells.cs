dd9a9acd25cb0d91d41582752681b53b4d3244d8
ChristCampos <christian.rocampos@gmail.com>
2024-06-17 20:13:48 -0300

    Projeto pronto para hospedagem

---
+++ Model/Spells.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysticPartyTracker.Models
+{
+    public class Spells
+    {
+        public string Magia { get; set; }
+        public string Tipo { get; set; }
+        public int Dano { get; set; }
+
+        public Spells(string magia, string tipo, int dano)
+        {
+            Magia = magia;
+            Tipo = tipo;
+            Dano = dano;
+        }
+    }
+}
---
+++ ViewModels/SpellsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MysticPartyTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MysticPartyTracker.ViewModels
+{
+    public partial class SpellsViewModel : ObservableObject
+    {
+        public SpellsViewModel()
+        {
+            CriarMagiaCommand = new Command(CriarMagia);
+        }
+
+        [ObservableProperty]
+        private string _magia;
+
+        [ObservableProperty]
+        private string _tipo;
+
+        [ObservableProperty]
+        private int _dano;
+
+        private ObservableCollection<Spells> _spells = new ObservableCollection<Spells>();
+        public ObservableCollection<Spells> Spells
+        {
+            get { return _spells; }
+            set { _spells = value; }
+        }
+
+        public ICommand CriarMagiaCommand { get; }
+
+        public void CriarMagia()
+        {
+            _spells.Add(new Spells(Magia, Tipo, Dano));
+        }
+    }
+}
